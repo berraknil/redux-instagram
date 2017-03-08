142c611a0a54e004da018a7c57ec3f9807c2c561
Berrak Nil <berraknil@gmail.com>
2017-03-08 18:53:38 +0300

    add all node stuff

---
+++ client/reduxstagram.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+
+import css from './styles/style.styl';
+
+// Import Components
+
+import Main from './components/Main';
+
+
+render(<Main/>, document.getElementById('root'));
